5a91105c21dc5ce49fe92fbba9b5bb64b744abaf
arunptl100 <arunptl100@hotmail.com>
2020-06-22 18:55:52 +0100

    parsed xml response

--- pmarket.py
+++ pmarket.py
@@ -1,19 +1,55 @@
 import xml.etree.ElementTree as ET
 import requests
 
+# False = Use a raw xml dump for testing purposes
+# True  = Use live api requests
+live = False
+
+# Zoopla API details
 api_token = 'kwgn93rerntytt8e5zy5zf84'
 api_url_base = 'https://api.zoopla.co.uk/api/v1/'
 parameters = {'api_key': api_token}
-
-
 url = api_url_base + 'property_listings'
-parameters['area'] = 'Leamington Spa'
 
-# response = requests.get(url=url, params=parameters)
-# print(response.text)
-tree = ET.parse('response_dump.xml')
-root = tree.getroot()
-# root = ET.fromstring(response.text)
+
+class properties:
+    def __init__(self):
+        self.properties = []
+
+    def addprop(self, xml_iter):
+        self.properties.append(xml_iter)
+
+    def print(self):
+        print("==========================================================")
+        for property in self.properties:
+            for child in property:
+                if child.tag == "agent_address":
+                    print("Address : " + child.text)
+                elif child.tag == "latitude":
+                    print("Latitude : " + child.text)
+                elif child.tag == "longitude":
+                    print("Longitude : " + child.text)
+                elif child.tag == "price":
+                    print("Price : " + "£" + child.text)
+            print("==========================================================")
+
+
+parameters['area'] = 'Leamington Spa'
+parameters['page_size'] = '100'
+
+if live:
+    response = requests.get(url=url, params=parameters)
+    root = ET.fromstring(response.text)
+else:
+    tree = ET.parse('response_dump.xml')
+    root = tree.getroot()
+
+props = properties()
 for listing in root.iter("listing"):
-    for child in listing.iter("agent_address"):
-        print(child.text)
+    props.addprop(listing)
+
+props.print()
+
+
+
+# whitespace
